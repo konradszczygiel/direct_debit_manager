33ee32f5f27cce86877fcf1f67d0839b82c285a6
Filip Kulig <filip.kulig@performancehorizon.com>
2018-01-20 19:30:06 +0000

    Some basic template system.

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+$footer = '
+<footer class="footer">
+	<div class="container">
+		<span class="text-muted">Direct Debit Manager. Copyright by Konrad</span>
+	</div>
+</footer>';
+
+echo $footer;
---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+
+$header = '
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Sticky Footer Navbar Template for Bootstrap</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="public/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="public/css/sticky-fotter-navbar.css" rel="stylesheet">
+</head>';
+
+echo $header;
+
+
+?>
