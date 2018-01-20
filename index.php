d64cb8c722a0720ac518f64926e9e04177d6952e
Filip Kulig <filip.kulig@performancehorizon.com>
2018-01-20 19:47:07 +0000

    Add skeleton pages.

--- index.php
+++ index.php
@@ -8,19 +8,26 @@ require_once 'header.php';
 
 <?php require_once 'menu.php'; ?>
 
-<!-- Begin page content -->
 <main role="main" class="container">
-	<h1 class="mt-5">Sticky footer with fixed navbar</h1>
-	<p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
-	<p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+
+<?php
+
+    $action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+    $fileToLoad = 'pages/' . $action . '.php';
+
+    if (file_exists($fileToLoad)) {
+        $content = file_get_contents($fileToLoad);
+        echo $content;
+    }
+
+?>
+
 </main>
 
 
 <?php require_once 'footer.php'; ?>
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="public/js/popper.min.js"></script>
 <script src="public/js/bootstrap.min.js"></script>
--- menu.php
+++ menu.php
@@ -11,10 +11,10 @@ $header = '
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item active">
-					<a class="nav-link" href="/add">Dodaj nowy <span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="/index.php?action=add">Dodaj nowy <span class="sr-only">(current)</span></a>
 				</li>
 			</ul>
-			<form class="form-inline mt-2 mt-md-0">
+			<form class="form-inline mt-2 mt-md-0" method="post" action="index.php?action=search">
 				<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 			</form>
