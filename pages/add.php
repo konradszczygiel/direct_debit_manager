5eba9369b616376a92e1b27b6ec0c74fb8d0ca4f
Filip Kulig <filip.kulig@performancehorizon.com>
2018-01-24 21:16:29 +0000

    Make page great again.

--- pages/add.php
+++ pages/add.php
@@ -1,8 +1,8 @@
 <?php
 
-$name = $_POST["nazwa_dd"];
-$amount = $_POST["kwota_dd"];
-$data = $_POST["data_dd"];
+$name = isset($_POST["nazwa_dd"]) ? $_POST['nazwa_dd'] : '';
+$amount = isset($_POST["kwota_dd"]) ? $_POST['kwota_dd'] : '';
+$data = isset($_POST["data_dd"]) ? $_POST['data_dd'] : '';
 
 
 echo 'Nazwa: ' . $name . ' <br> Kwota:  ' . $amount . '<br> Data: ' . $data;
